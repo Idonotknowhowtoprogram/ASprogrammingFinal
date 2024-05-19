3e59f78e636cb30721c80f93783e149eea8a13a4
Michael Taylor <88051898+ThePancake41@users.noreply.github.com>
2024-05-19 14:37:36 -0700

    Update mysautility.ts
    Added the test branch where the code that is half finished will be. This code is the start of how we will finalize our web scraping.

--- mysautility.ts
+++ mysautility.ts
@@ -1,12 +1,16 @@
-// Name: MySAUtility
+// Name: mysautility
 
 import "@johnlindquist/kit"
 import { Value } from "@johnlindquist/kit/core/enum";
 
+const password = await arg("input your password");
+var mode = await arg("assignment", "grade", "general");
 const puppeteer = await npm('puppeteer');
 const browser = await puppeteer.launch({
   headless: false
 });
+const page = await browser.newPage();
+
 
 async function clickNextLogin(currentPage) {
     await currentPage.waitForSelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b', {
@@ -20,8 +24,6 @@ async function clickNextLogin(currentPage) {
 
 //Login
 (async () => {
-var goodClose = false;
-const page = await browser.newPage();
 await page.goto('https://sonomaacademy.myschoolapp.com/app#login');
 await page.waitForSelector('.form-control', {
     visible: true,
@@ -31,67 +33,71 @@ await page.click('.btn-lg');
 await page.waitForNetworkIdle();
 clickNextLogin(page);
 setTimeout(() => {
-    page.type('.whsOnd.zHQkBf', 'password')
+    page.type('.whsOnd.zHQkBf', password)
     setTimeout(() => {
         clickNextLogin(page)
-        page.screenshot({path: '/screenshot.png'})
     }, 3000)
 }, 6000);
-})();
 
-//Web Scraping
-async function assignmentModeScrape() {
-    await page.click("#showHideGrade > div > label.btn.btn-default.btn-sm.bold.active > span");
+// Web Scraping Stuff
+setTimeout(async () => {
+  //Grades
+  //var grade1 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(1) > div.col-md-2.standard-padding-needed > h3").innerHTML});
+
+  //var grade2 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(2) > div.col-md-2.standard-padding-needed > h3").innerHTML});
   
-    var grade1 = document.querySelector("#coursesContainer > div:nth-child(1) > div.col-md-2.standard-padding-needed > h3");
+  //var grade3 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(3) > div.col-md-2.standard-padding-needed > h3").innerHTML});
+
+  //var grade4 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(4) > div.col-md-2.standard-padding-needed > h3").innerHTML});
   
-    var grade2 = document.querySelector("#coursesContainer > div:nth-child(2) > div.col-md-2.standard-padding-needed > h3");
-  
-    var grade3 = document.querySelector("#coursesContainer > div:nth-child(3) > div.col-md-2.standard-padding-needed > h3");
-  
-    var grade4 = document.querySelector("#coursesContainer > div:nth-child(4) > div.col-md-2.standard-padding-needed > h3");
-  
-    var class1 = document.querySelector("#coursesContainer > div:nth-child(1) > div:nth-child(1) > a > h3");
-  
-    var class2 = document.querySelector("#coursesContainer > div:nth-child(2) > div:nth-child(1) > a > h3");
-  
-    var class3 = document.querySelector("#coursesContainer > div:nth-child(3) > div:nth-child(1) > a > h3");
-  
-    var class4 = document.querySelector("#coursesContainer > div:nth-child(4) > div:nth-child(1) > a > h3");
-  
-    var class1ActiveAssignments = document.querySelector("#coursesContainer > div:nth-child(1) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span");
-  
-    var class2ActiveAssignments = document.querySelector("#coursesContainer > div:nth-child(2) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span");
-  
-    var class3ActiveAssignments = document.querySelector("#coursesContainer > div:nth-child(3) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span");
-  
-    var class4ActiveAssignments = document.querySelector("#coursesContainer > div:nth-child(4) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span");
-  
-  }
-  if (mode == "assignment") {
-    assignmentModeScrape();
-    await div(`
-    <body>
-      <h2>${class1}</h2>
-      <class1>
-        <a>Your grade is ${grade1}</a>
-        <a>You have ${class1ActiveAssignments} active assignments</a>
-      </class1>
-      <h2>${class2}</h2>
-      <class2>
-        <a>Your grade is ${grade2}</a>
-        <a>You have${class2ActiveAssignments} active assignments</a>
-      </class2>
-      <h2>${class3}</h2>
-      <class3>
-        <a>Your grade is ${grade3}</a>
-        <a>You have ${class3ActiveAssignments} active assignments</a>
-      </class3>
-      <h2>${class4}</h2>
-      <class4>
-        <a>Your grade is ${grade4}</a>
-        <a>You have ${class4ActiveAssignments} active assignments</a>
-      </class4>
-    </body>
-  </html>`);
-  }
+  //Classes
+  var class1 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(1) > div:nth-child(1) > a > h3").innerHTML});
+    
+  var class2 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(2) > div:nth-child(1) > a > h3").innerHTML});
+    
+  var class3 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(3) > div:nth-child(1) > a > h3").innerHTML});
+    
+  //var class4 = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(4) > div:nth-child(1) > a > h3").innerHTML});
+
+  await div(`
+  <body>
+    <h2>${class1},  ${class2}, ${class3}</h2>
+    <h2></h2>
+  </body>
+</html>`);
+  /*
+  var class1ActiveAssignments = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(1) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span").innerHTML});
+    
+  var class2ActiveAssignments = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(2) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span").innerHTML});
+    
+  var class3ActiveAssignments = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(3) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span").innerHTML});
+    
+  var class4ActiveAssignments = await page.evaluate(() => {return document.querySelector("#coursesContainer > div:nth-child(4) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span").innerHTML});
+
+        await div(`
+        <body>
+          <h2>${class1}</h2>
+          <class1>
+            <a>Your grade is ${grade1}</a>
+            <a>You have ${class1ActiveAssignments} active assignments</a>
+          </class1>
+          <h2>${class2}</h2>
+          <class2>
+            <a>Your grade is ${grade2}</a>
+            <a>You have${class2ActiveAssignments} active assignments</a>
+          </class2>
+          <h2>${class3}</h2>
+          <class3>
+            <a>Your grade is ${grade3}</a>
+            <a>You have ${class3ActiveAssignments} active assignments</a>
+          </class3>
+          <h2>${class4}</h2>
+          <class4>
+            <a>Your grade is ${grade4}</a>
+            <a>You have ${class4ActiveAssignments} active assignments</a>
+          </class4>
+        </body>
+      </html>`);
+      */
+}, 20000)
+})();
