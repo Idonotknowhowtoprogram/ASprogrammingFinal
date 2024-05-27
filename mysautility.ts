4acdc7c11270c990e05844a11cd8599ebeeec17f
Michael Taylor <88051898+ThePancake41@users.noreply.github.com>
2024-05-27 16:59:43 -0700

    Update mysautility.ts
    This works!

--- mysautility.ts
+++ mysautility.ts
@@ -1,101 +1,74 @@
-// Name: MySAUtility
+// Name: mysautility
 
 import "@johnlindquist/kit"
 import { Value } from "@johnlindquist/kit/core/enum";
 
+//const password = await arg("input your password");
+const password = "password"
+const mode = await arg("select mode", ["assignment", "grade", "general"]);
 const puppeteer = await npm('puppeteer');
 const browser = await puppeteer.launch({
   headless: false
 });
+const page = await browser.newPage();
 
 async function clickNextLogin(currentPage) {
-    await currentPage.waitForSelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b', {
-        visible: true,
-        clickable: true
-    });
-    await currentPage.evaluate(() => {console.log("button found")});
-    await currentPage.evaluate(() => {(<HTMLElement> document.querySelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b')).click()});
-    await currentPage.evaluate(() => {console.log("button clicked")});
+  await currentPage.waitForSelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b', {
+    visible: true,
+    clickable: true
+  });
+  await currentPage.evaluate(() => { (<HTMLElement>document.querySelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b')).click() });
+}
+
+//Web Scraping
+var numOfCourses = 0;
+var classes:{grade:string, href:string, name:string, activeAssignments:string}[] = []
+
+async function assignmentModeScrape() {
+  await page.waitForSelector("#showHideGrade > div > label:nth-child(2) > span", {
+    visible: true,
+    clickable: true
+  })
+  await page.click("#showHideGrade > div > label:nth-child(2) > span");
+  var things = await page.evaluate(() => {
+    let subElements = Array.from(document.getElementById("coursesContainer").children).length;
+    let newThings = []
+    for (let i = 0; i <= subElements; i++) {
+      try {
+        let g = document.querySelector(`#coursesContainer > div:nth-child(${i}) > div.col-md-2.standard-padding-needed > h3`).innerHTML;
+        let n = document.querySelector(`#coursesContainer > div:nth-child(${i}) > div:nth-child(1) > a > h3`).innerHTML;
+        let aA = document.querySelector(`#coursesContainer > div:nth-child(${i}) > div:nth-child(3) > table > tbody > tr:nth-child(3) > td:nth-child(1) > span`).innerHTML;
+        newThings.push({"name": n,  "grade": g, "activeAssignments": aA});
+      } catch {
+        console.log("error  :(");
+      }
+    }
+    return newThings;
+  })
+  console.log(things)
+  return things;
 }
 
 //Login
 (async () => {
-var goodClose = false;
-const page = await browser.newPage();
 await page.goto('https://sonomaacademy.myschoolapp.com/app#login');
 await page.waitForSelector('.form-control', {
     visible: true,
   });
-await page.type('.form-control', 'firstname.lastname@sonomaacademy.org');
+await page.type('.form-control', 'first.last@sonomaacademy.org');
 await page.click('.btn-lg');
 await page.waitForNetworkIdle();
 clickNextLogin(page);
 setTimeout(() => {
-    page.type('.whsOnd.zHQkBf', 'password')
+    page.type('.whsOnd.zHQkBf', password)
     setTimeout(() => {
         clickNextLogin(page)
-        page.screenshot({path: '/screenshot.png'})
     }, 3000)
 }, 6000);
+
+//Web Scraping function calls and output
+if (mode == "assignment") {
+  var classInfo = await assignmentModeScrape();
+  await div(md(`${classInfo}`))
+}
 })();
-
-//Web Scraping
-async function assignmentModeScrape() {
-    await page.click("#showHideGrade > div > label.btn.btn-default.btn-sm.bold.active > span");
-    
-    var courseElements = [];
-
-    var divChildren = Array.from(document.getElementById('coursesContainer').children);
-    for (let i = 0; i < divChildren.length; i++) {
-        if (divChildren[i].tagName === 'DIV') {
-            courseElements.push(Array.from(divChildren[i].children as ChildNode[]));
-        }
-    }
-
-    var numOfCourses = courseElements.length;
-    console.log(courseElements);
-    var classes = [];
-
-    for (let i = 0; i < numOfCourses; i++) {
-        var classObj = {};
-        var element = courseElements[i][0];
-        var aElement = element.querySelector('a');
-        if (aElement) {
-            classObj.href = aElement.getAttribute('href');
-            var courseName = aElement.querySelector('h3');
-            if (courseName) {
-                classObj.name = courseName.textContent;
-            }
-        }
-        classes.push(classObj);
-    }
-
-
-  }
-  if (mode == "assignment") {
-    assignmentModeScrape();
-    await div(`
-    <body>
-      <h2>${class1}</h2>
-      <class1>
-        <a>Your grade is ${grade1}</a>
-        <a>You have ${class1ActiveAssignments} active assignments</a>
-      </class1>
-      <h2>${class2}</h2>
-      <class2>
-        <a>Your grade is ${grade2}</a>
-        <a>You have${class2ActiveAssignments} active assignments</a>
-      </class2>
-      <h2>${class3}</h2>
-      <class3>
-        <a>Your grade is ${grade3}</a>
-        <a>You have ${class3ActiveAssignments} active assignments</a>
-      </class3>
-      <h2>${class4}</h2>
-      <class4>
-        <a>Your grade is ${grade4}</a>
-        <a>You have ${class4ActiveAssignments} active assignments</a>
-      </class4>
-    </body>
-  </html>`);
-  }
