2aadfb18c02177ef655a8960aab8466d6aa80bdc
Michael Taylor <88051898+ThePancake41@users.noreply.github.com>
2024-05-26 21:03:19 -0700

    Update mysautility.ts
    This is the current test code, in the middle of debug but idkhtp might be able to use it. -_-

--- mysautility.ts
+++ mysautility.ts
@@ -3,87 +3,58 @@
 import "@johnlindquist/kit"
 import { Value } from "@johnlindquist/kit/core/enum";
 
-const password = await arg("input your password");
-var mode = await arg("assignment", "grade", "general");
+//const password = await arg("input your password");
+const password = "password"
+const mode = await arg("select mode", ["assignment", "grade", "general"]);
 const puppeteer = await npm('puppeteer');
 const browser = await puppeteer.launch({
   headless: false
 });
 const page = await browser.newPage();
 
-
 async function clickNextLogin(currentPage) {
   await currentPage.waitForSelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b', {
     visible: true,
     clickable: true
   });
-  await currentPage.evaluate(() => { console.log("button found") });
   await currentPage.evaluate(() => { (<HTMLElement>document.querySelector('.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.BqKGqe.Jskylb.TrZEUc.lw1w4b')).click() });
-  await currentPage.evaluate(() => { console.log("button clicked") });
 }
-
-//Login
-(async () => {
-
-await page.goto('https://sonomaacademy.myschoolapp.com/app#login');
-await page.waitForSelector('.form-control', {
-    visible: true,
-  });
-await page.type('.form-control', 'firstname.lastname@sonomaacademy.org');
-await page.click('.btn-lg');
-await page.waitForNetworkIdle();
-clickNextLogin(page);
-setTimeout(() => {
-    page.type('.whsOnd.zHQkBf', password)
-    setTimeout(() => {
-        clickNextLogin(page)
-    }, 3000)
-}, 6000);
-
-// Web Scraping Stuff
-setTimeout(async () => {
-  await div(`
-  <body>
-    <h2>${class1},  ${class2}, ${class3}</h2>
-    <h2></h2>
-  </body>
-</html>`);
-}, 20000)
-})();
-  var goodClose = false;
-  const page = await browser.newPage();
-  await page.goto('https://sonomaacademy.myschoolapp.com/app#login');
-  await page.waitForSelector('.form-control', {
-    visible: true,
-  });
-  await page.type('.form-control', 'firstname.lastname@sonomaacademy.org');
-  await page.click('.btn-lg');
-  await page.waitForNetworkIdle();
-  clickNextLogin(page);
-  setTimeout(() => {
-    page.type('.whsOnd.zHQkBf', 'password')
-    setTimeout(() => {
-      clickNextLogin(page)
-      page.screenshot({ path: '/screenshot.png' })
-    }, 3000)
-  }, 6000);
-})();
 
 //Web Scraping
 let numOfCourses = 0;
 let classes:{grade:string, href:string, name:string, activeAssignments:string}[] = []
+
 async function assignmentModeScrape() {
-  await page.click("#showHideGrade > div > label.btn.btn-default.btn-sm.bold.active > span");
+  await page.waitForSelector("#showHideGrade > div > label:nth-child(2) > span", {
+    visible: true,
+    clickable: true
+  })
+  await page.click("#showHideGrade > div > label:nth-child(2) > span");
 
   var courseElements:HTMLCollection[] = [];
+  const divChildren = await page.evaluate(() => {
+    const container = document.getElementById('coursesContainer');
+    return Array.from(container ? container.children : []);
+  });
 
-  var divChildren = Array.from(document.getElementById('coursesContainer')!.children);
-  for (let i = 0; i < divChildren.length; i++) {
-    courseElements.push(divChildren[i].children);
-  }
-
+  console.log("after disaster");
+  console.log(divChildren);
+  let thing1 = "not triggered";
+  await new Promise((resolve, reject) => {
+    try {
+      for (let i = 0; i < divChildren.length; i++) {
+        courseElements.push(divChildren[i]!.children);
+        thing1 += " triggered :) ";
+      }
+      resolve("It is done. -_-");
+    } catch (error) {
+      reject(error);
+    }
+  });
+  console.log(thing1);
+  /*
   numOfCourses = courseElements.length;
-  
+
   for (let i = 0; i < numOfCourses; i++) {
     var element = courseElements[i][0];
     var classObj = {
@@ -92,7 +63,7 @@ async function assignmentModeScrape() {
       name: "error",
       activeAssignments: "error"
     };
-    var gradeDiv = element.children[3]
+    var gradeDiv = element.children[3];
     var gradeNode = gradeDiv!.firstChild!.textContent!;
     classObj.grade = gradeNode;
     var linkNode = element.children[0];
@@ -105,15 +76,33 @@ async function assignmentModeScrape() {
     }
     classes.push(classObj);
 }
-
-
+*/
 }
+
+//Login
+(async () => {
+await page.goto('https://sonomaacademy.myschoolapp.com/app#login');
+await page.waitForSelector('.form-control', {
+    visible: true,
+  });
+await page.type('.form-control', 'first.last@sonomaacademy.org');
+await page.click('.btn-lg');
+await page.waitForNetworkIdle();
+clickNextLogin(page);
+setTimeout(() => {
+    page.type('.whsOnd.zHQkBf', password)
+    setTimeout(() => {
+        clickNextLogin(page)
+    }, 3000)
+}, 6000);
+
+//Web Scraping function calls and output
 if (mode == "assignment") {
   assignmentModeScrape();
   let htmlString = `
   <body>
 `;
-
+/*
 for (let i = 0; i < numOfCourses; i++) {
   htmlString += `
     <h2>${classes[i].name}</h2>
@@ -127,5 +116,7 @@ htmlString += `
   </body>
 </html>
 `;
-  await div(htmlString);
+  //await div(htmlString);
+ */
 }
+})();
